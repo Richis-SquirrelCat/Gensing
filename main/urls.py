6fc0025616fc989dbbf72dfcd562e2c91ee9d488
Richis-SquirrelCat <126805009+Richis-SquirrelCat@users.noreply.github.com>
2023-03-06 13:11:44 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('start/', start),
     path('product/', product),
     path('post-order/', post_order),
+    path('get-order/', get_order),
     path('discount/', discount),
     path('about-product/', about_product),
     path('about-company/', about_company),
     path('using/', using),
     path('faq/', faq),
-    path('product-cooking/', product_cooking),
-    path('recommend/', recommend),
+    path('product-cooking/', how_using),
     path('info/', info),
+    path('info-social/', info_social)
 ]
