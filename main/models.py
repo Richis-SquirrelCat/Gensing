3dc5d811dc48056613d8de7582698d826bc1c096
Richis-SquirrelCat <126805009+Richis-SquirrelCat@users.noreply.github.com>
2023-03-06 13:11:21 +0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,87 +1,64 @@
 from django.db import models
 
 
-class Start(models.Model):
-    title = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to='background')
-    button = models.CharField(max_length=255)
+class Info(models.Model):
+    logo = models.ImageField(upload_to='logo/')
     text_uz = models.CharField(max_length=255)
     text_ru = models.CharField(max_length=255)
+
+
+class SocialMedia(models.Model):
+    img = models.ImageField(null=True, blank=True, upload_to="social_media/")
+    link = models.URLField()
 
 
 class Order(models.Model):
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
-    text_uz = models.CharField(max_length=255)
-    text_ru = models.CharField(max_length=255)
 
 
 class Discount(models.Model):
-    title = models.CharField(max_length=255)
     first_cost = models.IntegerField()
     last_coat = models.IntegerField()
     benefit = models.IntegerField()
+    foto = models.ImageField(upload_to='discount/')
     text_uz = models.CharField(max_length=255)
     text_ru = models.CharField(max_length=255)
 
 
 class Product(models.Model):
     photo = models.ImageField(upload_to='product/')
-    name = models.CharField(max_length=255)
     cost = models.IntegerField()
-    text_uz = models.CharField(max_length=255)
-    text_ru = models.CharField(max_length=255)
+    name_uz = models.CharField(max_length=255)
+    name_ru = models.CharField(max_length=255)
 
 
-class About_product(models.Model):
-    text = models.CharField(max_length=255)
+class AboutProduct(models.Model):
     photo = models.ImageField(upload_to='product/')
     text_uz = models.CharField(max_length=255)
     text_ru = models.CharField(max_length=255)
 
 
-class Product_cooking(models.Model):
-    text = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to='product/')
+class AboutCompany(models.Model):
+    title_uz = models.CharField(max_length=255)
+    title_ru = models.CharField(max_length=255)
+    text_uz = models.TextField()
+    text_ru = models.TextField()
+
+
+class WhoUse(models.Model):
     text_uz = models.CharField(max_length=255)
     text_ru = models.CharField(max_length=255)
 
 
-class About_company(models.Model):
-    text = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to='company/')
-    text_uz = models.CharField(max_length=255)
-    text_ru = models.CharField(max_length=255)
-
-
-class Recommend(models.Model):
-    title = models.CharField(max_length=255)
-    foto = models.ImageField(upload_to='rec/')
-    text = models.CharField(max_length=255)
-    text_uz = models.CharField(max_length=255)
-    text_ru = models.CharField(max_length=255)
-
-
-class Using(models.Model):
-    title = models.CharField(max_length=255)
-    text = models.CharField(max_length=255)
-    text_uz = models.CharField(max_length=255)
-    text_ru = models.CharField(max_length=255)
+class HowToUse(models.Model):
+    title_uz = models.CharField(max_length=255)
+    title_ru = models.CharField(max_length=255)
+    text_uz = models.TextField()
+    text_ru = models.TextField()
 
 
 class Faq(models.Model):
     number = models.CharField(max_length=255)
-    text = models.CharField(max_length=255)
-    text_uz = models.CharField(max_length=255)
-    text_ru = models.CharField(max_length=255)
-
-
-class Info(models.Model):
-    logo = models.ImageField(upload_to='logo/')
-    address = models.CharField(max_length=255)
-    website = models.CharField(max_length=255)
-    website2 = models.CharField(max_length=255)
-    website3 = models.CharField(max_length=255)
-    website4 = models.CharField(max_length=255)
     text_uz = models.CharField(max_length=255)
     text_ru = models.CharField(max_length=255)
