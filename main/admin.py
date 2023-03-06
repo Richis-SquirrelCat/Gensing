70e1e81e7f0fcc65c65e4659ff5d04ccb9c21d0e
Richis-SquirrelCat <126805009+Richis-SquirrelCat@users.noreply.github.com>
2023-03-06 13:17:22 +0300

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
 
-# admin.site.register(Start)
-admin.site.register(Start)
+admin.site.register(SocialMedia)
 admin.site.register(Order)
 admin.site.register(Discount)
 admin.site.register(Product)
-admin.site.register(About_product)
-admin.site.register(Product_cooking)
-admin.site.register(About_company)
-admin.site.register(Recommend)
-admin.site.register(Using)
+admin.site.register(AboutProduct)
+admin.site.register(AboutCompany)
+admin.site.register(WhoUse)
+admin.site.register(HowToUse)
 admin.site.register(Faq)
 admin.site.register(Info)
